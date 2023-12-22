b83b7d21ac8c16eb28b50a4a7c06a1497888e517
davkaforce <davaadalaio@gmail.com>
2023-12-22 13:55:55 +0800

    commit

--- mycalculator/calculator.js
+++ mycalculator/calculator.js
@@ -4,13 +4,14 @@ const root = document.getElementById("root");
 
 const display = document.createElement("div");
 display.setAttribute("class", "display");
+
 root.appendChild(display);
 
 const grid = document.createElement("div");
 grid.setAttribute("class", "grid");
 root.appendChild(grid);
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 19; i++) {
   const input = document.createElement("button");
   input.setAttribute("class", "input");
   input.setAttribute("id", `input${i}`);
@@ -20,6 +21,7 @@ for (let i = 0; i < 20; i++) {
 // assigning value to inputs
 
 const myInputs = document.querySelectorAll(".input");
+
 myInputs[0].innerText = "AC";
 myInputs[1].innerText = "+/-";
 myInputs[2].innerText = "%";
@@ -37,26 +39,65 @@ myInputs[13].innerText = "2";
 myInputs[14].innerText = "3";
 myInputs[15].innerText = "+";
 myInputs[16].innerText = "0";
-myInputs[17].innerText = "0";
-myInputs[18].innerText = ".";
-myInputs[19].innerText = "=";
+myInputs[17].innerText = ".";
+myInputs[18].innerText = "=";
 
 //global variables
 
-let numberA = 2;
-let numberB = 10;
-let operation = "/";
+let numberA = 0;
+let numberB = 0;
+let operation = "";
 
 // send elements using event listener :
 // receive as global variables
 
-//numbers
-myInputs[0].addEventListener("click", function () {
-  operation = myInputs[0].innerText;
-  console.log(operation);
-});
+//inputs
+
+// click on numbers
+// creat
+
+for (let i = 0; i < myInputs.length; i++) {
+  myInputs[i].addEventListener("click", function () {
+    if (
+      //numbers
+      i == 16 ||
+      (7 > i && i > 3) ||
+      (11 > i && i > 7) ||
+      (15 > i && i > 11)
+    ) {
+      numberA = numberA * 10 + Number(myInputs[i].innerText);
+      console.log(numberA);
+      console.log(typeof numberA);
+      displayNumber.innerText = numberA;
+      //operations
+    } else if (i == 3 || i == 7 || i == 11 || i == 15) {
+      operation = myInputs[i].innerText;
+      console.log(operation);
+      numberB = numberA;
+      numberA = 0;
+    } else if (i == 18) {
+      numberA = calculate();
+      displayNumber.innerText = numberA;
+    } else if (i == 0) {
+      numberA = 0;
+      displayNumber.innerText = numberA;
+    } else if (i == 1) {
+      numberA = numberA * -1;
+      displayNumber.innerText = numberA;
+    } else if (i == 2) {
+      numberA = numberA / 100;
+      displayNumber.innerText = numberA;
+    } else if (i == 17) {
+    }
+  });
+}
 
 // using global variables to display numbers and operation
+
+console.log(numberA);
+const displayNumber = document.createElement("p");
+displayNumber.innerText = numberA;
+display.appendChild(displayNumber);
 
 // operation boxes + addevent listener
 // number boxes + addevent listener
@@ -67,9 +108,9 @@ function calculate() {
   if (operation == "+") {
     return numberA + numberB;
   } else if (operation == "-") {
-    return numberA - numberB;
+    return numberB - numberA;
   } else if (operation == "/") {
-    return numberA / numberB;
+    return numberB / numberA;
   } else if (operation == "*") {
     return numberA * numberB;
   }
