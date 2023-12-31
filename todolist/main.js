8a42bddbfd0a637bdb80dc09836ad4859e84ccfa
davkaforce <davaadalaio@gmail.com>
2023-12-31 15:02:43 +0800

    dragdrop and localstorage

--- todolist/main.js
+++ todolist/main.js
@@ -7,12 +7,10 @@ const root = document.getElementById("root");
 let currentBoardButton = 0;
 
 for (let i = 0; i < 4; i++) {
-  const board = document.createElement("div");
-  board.setAttribute("class", "boards");
-  board.setAttribute("id", `board${i + 1}`);
-  root.appendChild(board);
+  const board = document.querySelector(`#board${i + 1}`);
 
   const paragraph = document.createElement("p");
+  paragraph.setAttribute("id", `paragraph${i + 1}`);
   paragraph.innerText = `My tasks`;
   board.appendChild(paragraph);
 
@@ -21,12 +19,10 @@ for (let i = 0; i < 4; i++) {
   button.innerText = "Add task";
   board.appendChild(button);
 
-  const cards = document.createElement("div");
-  cards.setAttribute("class", "cards");
-  cards.setAttribute("id", `cards${i + 1}`);
+  const cards = document.querySelector(`#cards${i + 1}`);
   board.appendChild(cards);
 
-  // add new task button for the board
+  // add new task button in the board
   button.addEventListener("click", () => {
     modal.style.display = "block";
     taskName.value = "";
@@ -42,12 +38,22 @@ let myArrayTask2 = [];
 let myArrayTask3 = [];
 let myArrayTask4 = [];
 
+let AllArrays = [myArrayTask1, myArrayTask2, myArrayTask3, myArrayTask4];
+
+let myLocalStringArray1 = [];
+let myLocalStringArray2 = [];
+let myLocalStringArray3 = [];
+let myLocalStringArray4 = [];
+
 let cards1 = document.getElementById("cards1");
 let cards2 = document.getElementById("cards2");
 let cards3 = document.getElementById("cards3");
 let cards4 = document.getElementById("cards4");
 
-// new popup window creation(HTML/CSS) for new task
+let dragTaskArray;
+let dragTaskIndex;
+
+// "new task" popup window creation
 
 const modal = document.createElement("div");
 modal.setAttribute("class", "modal");
@@ -69,7 +75,7 @@ inButton.setAttribute("class", "inButton");
 inButton.innerText = "Enter";
 modalContent.appendChild(inButton);
 
-// "edit task" window  (excluding functionality)
+// "edit task" popup window creation
 
 const modalEdit = document.createElement("div");
 modalEdit.setAttribute("class", "modal");
@@ -91,7 +97,7 @@ inButtonEdit.setAttribute("class", "inButton");
 inButtonEdit.innerText = "Save";
 modalContentEdit.appendChild(inButtonEdit);
 
-// declaring function for adding new task and render the board
+// declaring "render" function for adding new task and reset the tasks
 
 function render(array, cards) {
   cards.innerHTML = "";
@@ -111,13 +117,23 @@ function render(array, cards) {
     const taskDiv = document.createElement("div");
     taskDiv.setAttribute("class", "taskDiv");
     taskDiv.setAttribute("id", `task${index}`);
-
     cards.appendChild(taskDiv);
+    //drag and drop section
+    taskDiv.setAttribute("draggable", "true");
+    taskDiv.addEventListener("dragstart", () => {
+      taskDiv.classList.add("dragging");
+      dragTaskIndex = index;
+      dragTaskArray = AllArrays.indexOf(array);
+      // console.log(dragTaskArray, dragTaskIndex);
+    });
+    taskDiv.addEventListener("dragend", () => {
+      taskDiv.classList.remove("dragging");
+    });
 
     const existingTaskName = document.createElement("p");
     existingTaskName.innerText = "Task name: " + element.myTask;
     const existingTaskDesc = document.createElement("p");
-    existingTaskDesc.innerText = "Task deskcription: " + element.myDesc;
+    existingTaskDesc.innerText = "Task description: " + element.myDesc;
 
     taskDiv.appendChild(existingTaskName);
     taskDiv.appendChild(existingTaskDesc);
@@ -159,9 +175,20 @@ function render(array, cards) {
 
   modal.style.display = "none";
   currentBoardButton = 0;
+
+  // saving to localstorage
+  myLocalStringArray1 = JSON.stringify(myArrayTask1);
+  myLocalStringArray2 = JSON.stringify(myArrayTask2);
+  myLocalStringArray3 = JSON.stringify(myArrayTask3);
+  myLocalStringArray4 = JSON.stringify(myArrayTask4);
+
+  localStorage.setItem("myLocalStorageArray1", myLocalStringArray1);
+  localStorage.setItem("myLocalStorageArray2", myLocalStringArray2);
+  localStorage.setItem("myLocalStorageArray3", myLocalStringArray3);
+  localStorage.setItem("myLocalStorageArray4", myLocalStringArray4);
 }
 
-// add new task button in popup window
+// "add new task" button in popup window
 
 inButton.addEventListener("click", () => {
   console.log("check", currentBoardButton);
@@ -188,3 +215,53 @@ window.onclick = function (event) {
     modalEdit.style.display = "none";
   }
 };
+
+// drag and drop section
+
+const cardsContainers = document.querySelectorAll(".cards");
+cardsContainers.forEach((container, index) => {
+  container.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    const draggable = document.querySelector(".dragging");
+    container.appendChild(draggable);
+  });
+
+  container.addEventListener("drop", (e) => {
+    e.preventDefault();
+    const draggable = document.querySelector(".dragging");
+    const sourceIndex = dragTaskIndex;
+    const sourceArray = AllArrays[dragTaskArray];
+    const destinationArray = AllArrays[index];
+
+    // Move the task from the source array to the destination array
+    const taskToMove = sourceArray[sourceIndex];
+    sourceArray.splice(sourceIndex, 1);
+    destinationArray.push(taskToMove);
+
+    // Re-render the tasks to reflect the changes
+    render(sourceArray, cardsContainers[dragTaskArray]);
+    render(destinationArray, cardsContainers[index]);
+  });
+});
+
+// Render using localstorage
+
+myLocalStringArray1 = localStorage.getItem("myLocalStorageArray1");
+myLocalStringArray2 = localStorage.getItem("myLocalStorageArray2");
+myLocalStringArray3 = localStorage.getItem("myLocalStorageArray3");
+myLocalStringArray4 = localStorage.getItem("myLocalStorageArray4");
+
+let myLocalArray1 = JSON.parse(myLocalStringArray1);
+let myLocalArray2 = JSON.parse(myLocalStringArray2);
+let myLocalArray3 = JSON.parse(myLocalStringArray3);
+let myLocalArray4 = JSON.parse(myLocalStringArray4);
+
+myArrayTask1 = myLocalArray1;
+myArrayTask2 = myLocalArray2;
+myArrayTask3 = myLocalArray3;
+myArrayTask4 = myLocalArray4;
+
+render(myArrayTask1, cards1);
+render(myArrayTask2, cards2);
+render(myArrayTask3, cards3);
+render(myArrayTask4, cards4);
